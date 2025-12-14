0bd0fe1f5a38f44a8923ae4529ef06267da82b2e
Pramod Prajapat <prajapatpramod474@gmail.com>
2025-12-14 08:45:47 +0530

    commit

---
+++ backend/src/middleware/upload.ts
@@ -0,0 +1,17 @@
+import multer from 'multer';
+import path from 'path';
+
+const storage = multer.diskStorage({
+  destination: 'uploads/',
+  filename: (_req, file, cb) => {
+    cb(
+      null,
+      Date.now() + path.extname(file.originalname)
+    );
+  }
+});
+
+export const upload = multer({
+  storage,
+  limits: { fileSize: 2 * 1024 * 1024 }, // 2MB
+});
