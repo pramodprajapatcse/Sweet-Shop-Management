13dc5d72fd6c7a050a53764ca1fac69722a7a953
Pramod Prajapat <prajapatpramod474@gmail.com>
2025-12-14 09:54:04 +0530

    Update upload.ts

--- backend/src/middleware/upload.ts
+++ backend/src/middleware/upload.ts
@@ -2,16 +2,13 @@ import multer from 'multer';
 import path from 'path';
 
 const storage = multer.diskStorage({
-  destination: 'uploads/',
+  destination: path.join(process.cwd(), 'uploads'),
   filename: (_req, file, cb) => {
-    cb(
-      null,
-      Date.now() + path.extname(file.originalname)
-    );
+    cb(null, Date.now() + path.extname(file.originalname));
   }
 });
 
 export const upload = multer({
   storage,
-  limits: { fileSize: 2 * 1024 * 1024 }, // 2MB
+  limits: { fileSize: 2 * 1024 * 1024 },
 });
